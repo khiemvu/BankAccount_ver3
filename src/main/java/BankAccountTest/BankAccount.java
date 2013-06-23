9987fb8037b59b92e0fa2537fbc1e12372f63748
Mr.KhiemVu <khiemvu.org@gmail.com>
2013-06-24 05:39:46 +0700

    testOpentAccount_red

---
+++ src/main/java/BankAccountTest/BankAccount.java
@@ -0,0 +1,14 @@
+package BankAccountTest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: All_in_one
+ * Date: 6/24/13
+ * Time: 5:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BankAccount {
+    public double getBalance() {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ src/main/java/BankAccountTest/BankAccountService.java
@@ -0,0 +1,16 @@
+package BankAccountTest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: All_in_one
+ * Date: 6/24/13
+ * Time: 5:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BankAccountService {
+    public static void setData(BankAccountDAO bankAccountDAO) {
+    }
+
+    public static void openAccount(String s) {
+    }
+}
