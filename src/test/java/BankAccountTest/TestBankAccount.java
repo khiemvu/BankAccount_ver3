9fdabba5cd576d74ec51bcd0de6a973dde43db70
Mr.KhiemVu <khiemvu.org@gmail.com>
2013-06-24 05:29:51 +0700

    init Kata3

---
+++ src/test/java/BankAccountTest/TestBankAccount.java
@@ -0,0 +1,12 @@
+package BankAccountTest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: All_in_one
+ * Date: 6/24/13
+ * Time: 5:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestBankAccount {
+
+}
