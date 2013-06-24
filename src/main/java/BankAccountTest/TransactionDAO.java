458937ac90411827a48391828bf04abdc3d48fdf
Mr.KhiemVu <khiemvu.org@gmail.com>
2013-06-24 13:34:11 +0700

    init test transaction

---
+++ src/main/java/BankAccountTest/TransactionDAO.java
@@ -0,0 +1,12 @@
+package BankAccountTest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: All_in_one
+ * Date: 6/24/13
+ * Time: 1:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TransactionDAO
+{
+}
---
+++ src/main/java/BankAccountTest/TransactionService.java
@@ -0,0 +1,15 @@
+package BankAccountTest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: All_in_one
+ * Date: 6/24/13
+ * Time: 1:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TransactionService
+{
+    public static void setupData(TransactionDAO transactionDAO)
+    {
+    }
+}
